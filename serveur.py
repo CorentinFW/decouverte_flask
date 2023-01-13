ac76728dd21d75b3478cce3696287d5cfa5ce414
Cefco David <corentinfw@gmail.com>
2023-01-13 13:16:44 +0100

    premier route

--- serveur.py
+++ serveur.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    print("hello")
+    return render_template('index.html')
+
+@app.route('/createur')
+def createur():
+    return render_template('createur.html')
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
